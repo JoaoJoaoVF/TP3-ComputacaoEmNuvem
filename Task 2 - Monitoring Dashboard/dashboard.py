058cd225e9adcd8e0a807ccbf0ec1072dac4b11a
João Vitor Ferreira <jvferreira.20@gmail.com>
2025-01-18 14:40:45 -0300

    Começando alguns arquivos

---
+++ Task 2 - Monitoring Dashboard/dashboard.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import redis
+import json
+
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+def fetch_data_from_redis():
+  key = 'joaoferreira-proj3-output'
+  data = redis_client.get(key)
+  if data:
+      return json.loads(data)
+  return {}
+
+def main():
+  st.title("Dashboard de Monitoramento")
+
+  data = fetch_data_from_redis()
+
+  if data:
+      st.header("Métricas de Rede e Memória")
+      st.write("Percentual de Egressão de Rede:", data.get("percent-network-egress"))
+      st.write("Percentual de Memória em Cache:", data.get("percent-memory-cached"))
+
+      st.header("Média Móvel de CPU")
+      cpu_keys = [key for key in data.keys() if key.startswith("avg-cpu")]
+      for key in cpu_keys:
+          st.write(f"{key}:", data[key])
+  else:
+      st.write("Nenhum dado disponível.")
+
+if __name__ == "__main__":
+  main()
